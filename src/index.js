7c0ed6b64589ea3921391e32242747595e7e50ca
william <william cuba>
2016-09-06 15:26:26 -0500

    add route for loginpass component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import LoginPass from './components/LoginPass'
-
 import './index.css';
 
 import { Router, Route, Link, browserHistory } from 'react-router'
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+    <Route path="/login" component={LoginPass}>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
