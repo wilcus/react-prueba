1c2f643345aac36517e6701b4eebe45fe9f4b50f
Your Name <you@example.com>
2016-09-06 16:32:35 -0500

    adding transf1

---
+++ src/components/Transf1.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+export default class Transf1 extends Component {
+
+	handleTextImporte() {
+
+	}
+
+	handleTextReference() {
+
+	}
+
+	handleTextAccountDebito() {
+
+	}
+
+	handleTextAccountCredito() {
+
+	}
+
+	render() {
+		return (
+			<div className="container">
+				
+         	 	<div className="row">
+         	 		<div className="center">
+
+				      	<div className="form-group">
+						  	<label htmlFor="cuentaCredito">CUENTA DÉBITO</label>
+						  	<input type="text" className="form-control" id="cuentaCredito"
+						  			placeholder="Selecciona una cuenta"
+						  			onChange={this.handleTextAccountDebito}
+						  	/>
+						</div>
+						
+						<div className="form-group">
+						  	<label htmlFor="cuentaDebito">CUENTA CRÉDITO</label>
+						  	<input type="text" className="form-control" id="cuentaDebito"
+						  			placeholder="Selecciona una cuenta"
+						  			onChange={this.handleTextAccountCredito}
+						  	/>
+						</div>
+						
+						<div className="form-group">
+						  	<label htmlFor="importe">IMPORTE </label>
+						  	<input type="text" className="form-control" id="importe"
+						  			placeholder="Ingresa el importe"
+						  			onChange={this.handleTextImporte}
+						  	/>
+						</div>
+						
+						<div className="form-group">
+						  	<label htmlFor="referencia">REFERENCIA</label>
+						  	<input type="text" class="form-control" id="referencia"
+						  			placeholder="Ingresa una referencia"
+						  			onChange={this.handleTextReference}
+
+						  	/>
+						</div>
+						
+				        
+				        
+
+				        
+				        
+				        <button type="submit" className="btn btn-primary button-bank"> Siguiente</button>
+
+
+				    </div>
+				</div>
+		    </div>
+		)
+	}
+
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import LoginPass from './components/LoginPass'
+import Transf1   from './components/Transf1'
+
 import './index.css';
 
 import { Router, Route, Link, browserHistory } from 'react-router'
@@ -10,7 +12,8 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
     </Route>
-    <Route path="/login" component={LoginPass}>
+    <Route path="/login" component={LoginPass}/>
+    <Route path="/transf1" component={Transf1}>
     </Route>
   </Router>,
   document.getElementById('root')
