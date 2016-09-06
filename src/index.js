5da0b5d2fa95551f2159763ff58b22560c3922fe
william <william cuba>
2016-09-06 16:28:20 -0500

    add header of trasferencia3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import LoginPass from './components/LoginPass'
+import Transferencia3 from './components/Transferencia3'
 import './index.css';
 
 import { Router, Route, Link, browserHistory } from 'react-router'
@@ -11,6 +12,8 @@ ReactDOM.render(
     <Route path="/" component={App}>
     </Route>
     <Route path="/login" component={LoginPass}>
+    </Route>
+    <Route path="/transferencia3" component={Transferencia3}>
     </Route>
   </Router>,
   document.getElementById('root')
